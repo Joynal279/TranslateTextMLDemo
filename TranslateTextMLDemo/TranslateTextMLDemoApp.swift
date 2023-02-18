cb0de9b9a157c1e494de5ac8b353c531c76bd3f0
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-18 23:48:16 +0600

    Initial Commit

---
+++ TranslateTextMLDemo/TranslateTextMLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateTextMLDemoApp.swift
+//  TranslateTextMLDemo
+//
+//  Created by Joynal Abedin on 18/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TranslateTextMLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
